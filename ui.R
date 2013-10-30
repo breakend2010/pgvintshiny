75ffe46cd94a91f6b13b65ee096127426f46893a
Tomáš Greif <tgr>
2013-10-30 16:20:04 +0100

    t

--- ui.R
+++ ui.R
@@ -10,8 +10,6 @@ shinyUI(pageWithSidebar(
           selectInput("yaxis","Y Axis:", "Loading..."),
           selectInput("group","Group:" , "Loading..."),
           tags$textarea(id="facets", rows=1, cols=40, "Default value")          
-          #selectInput("plot.type","Plot Type:", list(boxplot = "boxplot", histogram = "histogram", density = "density", bar = "bar")),
-          #checkboxInput("show.points", "show points", TRUE),
      ),	
      
      # output				
